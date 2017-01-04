f1f5a845e0c1f125d97e166bf0b6198e8ea12f64
Matthew Perry <thesedatedprince@gmail.com>
2017-01-04 15:26:43 +0000

    Updated with error exception for empty station

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,13 +2,17 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike
-  
+
+
   def release_bike
-      Bike.new
+    fail "No bikes present" unless @bike
+
+    @bike
   end
-  
+
   def dock(bike)
     @bike = bike
   end
-  
+
+
 end
