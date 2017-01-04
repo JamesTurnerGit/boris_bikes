045bebb49fb2d78a0b2c8b33d1651e0c300daf51
Matthew Perry <thesedatedprince@gmail.com>
2017-01-04 12:53:53 +0000

    docking station dock method changes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,6 +6,6 @@ describe  DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+  it { is_expected.to respond_to (:dock_bike).with(1).argument}
   
-  it { is_expected.to respond_to :dock_bike}
 end
