dd1a67c0ed107b51691f615d88fd80d502c5748f
James Turner <mail.jt@gmail.com>
2017-01-05 13:28:24 +0000

    start of step 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,12 +4,17 @@ describe  DockingStation do
   it { is_expected.to respond_to :release_bike}
   it { is_expected.to respond_to :bikes}
   it { is_expected.to respond_to(:dock).with(1).argument}
+  it { is_expected.to respond_to :report_broken}
 
   describe '#release_bike' do
+    before(:each) do
+      subject.dock(double(:bike))
+    end
     it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      expect(subject.release_bike.Class).to eq bike
+    end
+    it 'releases a working bike' do
+      expect(subject.release_bike).to be_working
     end
   end
 
@@ -20,16 +25,15 @@ describe  DockingStation do
   end
 
   describe '#dock' do
-
     describe 'raises an error if there\'s more than twenty bikes present'do
       it 'raises an error if there is too many bikes'do
-        subject.capacity.times{subject.dock Bike.new}
-        expect{subject.dock Bike.new}.to raise_error("Dock full")
+        subject.capacity.times{subject.dock double(:bike)}
+        expect{subject.dock double(:bike)}.to raise_error("Dock full")
       end
       it 'and counts broken_bikes in capacity' do
-        subject.capacity.times{subject.dock Bike.new}
+        subject.capacity.times{subject.dock double(:bike)}
         subject.report_broken
-        expect{subject.dock Bike.new}.to raise_error("Dock full")
+        expect{subject.dock double(:bike)}.to raise_error("Dock full")
       end
     end
   end
@@ -39,10 +43,9 @@ describe  DockingStation do
       expect(subject.broken_bikes.size).to eq 0
     end
     it 'adds bike to Broken Bikes' do
-      subject.dock(Bike.new)
+      subject.dock(double(:bike))
       subject.report_broken
       expect(subject.broken_bikes.size).to eq 1
     end
-    it {is_expected.to respond_to :report_broken}
   end
 end
